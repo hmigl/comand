1cb87977aa82d7b6fb5e05802533dc4e6a80f244
hmigl <88746406+hmigl@users.noreply.github.com>
2023-01-09 19:15:13 -0300

    feat: display program usage when args are invalid

--- src/encoder/encoder.c
+++ src/encoder/encoder.c
@@ -1,7 +1,12 @@
 #include "encoder.h"
 
+static void display_usage(void) {
+  fprintf(stderr, "Usage: ./encoder [TEXT]...\n");
+}
+
 int main(int argc, char **argv) {
   if (!has_valid_input(argc, argv)) {
+    display_usage();
     return 1;
   }
   return 0;
