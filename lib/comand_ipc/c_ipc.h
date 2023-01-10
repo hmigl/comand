4dbf70b7b1c140840bfb594f906cee4a2b1a1df8
hmigl <88746406+hmigl@users.noreply.github.com>
2023-01-10 11:21:10 -0300

    feat: create struct that will encapsulate all program's required info

--- lib/comand_ipc/c_ipc.h
+++ lib/comand_ipc/c_ipc.h
@@ -7,4 +7,12 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
+typedef struct comand_ipc_data data_t;
+struct comand_ipc_data {
+  char *data;
+  unsigned long total_bytes;
+  // unsigned long total_compressed_bytes;
+  // unsigned long time_to_decompress;
+};
+
 #endif // LIB_COMAND_IPC_C_IPC_H_
