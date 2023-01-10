c48f0d9134293048e24f3c1dbe137849a3e8758a
hmigl <88746406+hmigl@users.noreply.github.com>
2023-01-10 10:29:58 -0300

    feat: add and link 'libcomand_ipc' to encoder

--- src/encoder/encoder.h
+++ src/encoder/encoder.h
@@ -1,13 +1,14 @@
 #ifndef SRC_ENCODER_ENCODER_H_
-# define SRC_ENCODER_ENCODER_H_
+#define SRC_ENCODER_ENCODER_H_
 
+#include "c_ipc.h"
+#include <errno.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h>
-#include <stdio.h>
 
 // Validation
 bool has_valid_input(int argc, char **argv);
 
-#endif  // SRC_ENCODER_ENCODER_H_
+#endif // SRC_ENCODER_ENCODER_H_
